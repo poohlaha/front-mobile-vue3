8c24f5b3d82ab0cf31c5bf4ce404bd86f64d5554
poohlaha <smile.love.tao@gmil.com>
2024-12-10 20:55:01 +0800

    update(): 修改代码

--- src/communal/app/index.ts
+++ src/communal/app/index.ts
@@ -7,22 +7,11 @@ import 'lib-flexible'
 import '@assets/styles/skin/index.less'
 import store from '@stores/index'
 
-// UI 组件
-import 'vant/lib/index.css'
-
-import { Button, Toast, Field, CellGroup, Swipe, SwipeItem, Tabbar, TabbarItem } from 'vant'
-
 const app = createApp(App)
 app.use(store)
 
 // 注册 router
 app.use(router)
 
-// 注册UI组件
-app.use(Button).use(Toast).use(Field).use(CellGroup)
-app.use(Swipe)
-app.use(SwipeItem)
-app.use(Tabbar)
-app.use(TabbarItem)
 
 app.mount('#app')
